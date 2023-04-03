4faf7211141b74588b506bb37b90a727158abe06
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2023-04-02 21:23:23 -0300

    imports components for angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDialogComponent } from './components/user/user-dialog/user-dialog.component';
+import { UserTableComponent } from './components/user/user-table/user-table.component';
+import { ElementDialogComponent } from './shared/element-dialog/element-dialog.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LocalDateTimePipe } from './shared/pipe/local-date-time.pipe';
+import { LocalDatePipe } from './shared/pipe/local-date.pipe';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ElementDialogComponent,
+    UserDialogComponent,
+    UserTableComponent,
+    LocalDateTimePipe,
+    LocalDatePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    LocalDateTimePipe,
+    LocalDatePipe,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
